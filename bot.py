9a386b1276f49855d2d4f495b1b015ce83c0f72d
arch0tic <118679872+arch0tic@users.noreply.github.com>
2023-11-06 21:55:24 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,11 @@ from ip2geotools.databases.noncommercial import DbIpCity
 import nmap3
 import os
 import subprocess
+import requests
+import json
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
 
 bot = discord.Bot()
 
@@ -12,7 +17,22 @@ bot = discord.Bot()
 @bot.command(description="Sends the bot's latency.") # this decorator makes a slash command
 async def arch(ctx): # a slash command will be created with the name "ping"
     await ctx.respond(f"# Arch Linux is the best distro ever")
-@bot.command(description="declaratie de dragoste dragului nostru prieten Valentin")
-async def vali(ctx):
-    await ctx.respond("# <@886986362262286357> SUGE PULA LA TOT SERVERU")
+@bot.command(description="Inspiring quote")
+async def inspire(ctx):
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]["q"]
+    await ctx.respond(quote)
+@bot.event
+async def on_message(message):
+    if(message.content.startswith('inspire')):
+        print("yes")
+        response = requests.get("https://zenquotes.io/api/random")
+        json_data = json.loads(response.text)
+        quote = json_data[0]["q"]
+        await message.channel.send(quote)
+@bot.event
+async def on_ready():
+    activity = discord.Game(name="Arch BTW!")
+    await bot.change_presence(status=discord.Status.idle, activity=activity)
 bot.run("MTE0ODI1MjY4MDU2NTgyMTUwMA.GDX9QH.EU4aFYHblRkiaRQM-7aMwtZIv8DOc4lCDhKWBM")
